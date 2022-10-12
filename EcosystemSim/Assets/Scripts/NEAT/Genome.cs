7845a10f8a412bdf8ddc6903df92e38dd601a686
polarcube1 <polarcube1@gmail.com>
2022-10-12 18:18:51 -0400

    ✨ Added Mutation Methods

--- EcosystemSim/Assets/Scripts/NEAT/Genome.cs
+++ EcosystemSim/Assets/Scripts/NEAT/Genome.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-using System;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,7 +15,11 @@ public class Genome
     // PROPERTIES
     public List<ConnectionGene> getConnections{
         get{
-            return connections;
+            List<ConnectionGene> sortedConnections = new List<ConnectionGene>();
+            sortedConnections = connections.OrderBy(gene => gene.InnovationNumber).ToList();
+            sortedConnections.Reverse();
+
+            return sortedConnections;
         }
     }
     public List<NodeGene> getNodes {
@@ -34,7 +39,7 @@ public class Genome
     }
 
     // METHODS
-    public double distance(Genome g2) {
+    public double Distance(Genome g2) {
         Genome g1 = this;
 
         int highestInnovationGene1 = g1.getConnections[g1.getConnections.Count - 1].InnovationNumber;
@@ -109,7 +114,7 @@ public class Genome
             + (neat.C3 * weightDiff
             );
     }
-    public static Genome crossOver(Genome g1, Genome g2) {
+    public static Genome CrossOver(Genome g1, Genome g2) {
         Neat neat = g1.getNeat;
 
         Genome child = neat.EmptyGenome();
@@ -167,7 +172,130 @@ public class Genome
         return child;
     }
 
-    public void mutate() {
+    // %%% CLEAN THIS UP
+    public void Mutate() {
+        if (UnityEngine.Random.Range(0f, 1f) < 0.2f)
+        {
+            MutateLink();
+        }
+        if (UnityEngine.Random.Range(0f, 1f) < 0.2f)
+        {
+            MutateLinkToggle();
+        }
+        if (UnityEngine.Random.Range(0f, 1f) < 0.2f)
+        {
+            MutateNode();
+        }
+        if (UnityEngine.Random.Range(0f, 1f) < 0.2f)
+        {
+            MutateWeightRandom();
+        }
+        if (UnityEngine.Random.Range(0f, 1f) < 0.2f)
+        {
+            MutateWeightShift();
+        }
+    }
 
+    public void MutateLink()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            NodeGene a = GetRandomNode(nodes);
+            NodeGene b = GetRandomNode(nodes);
+
+            if (a.X == b.X)
+            {
+                continue;
+            }
+
+            ConnectionGene con;
+            if (a.X > b.X)
+            {
+                con = new ConnectionGene(a, b);
+            }else
+            {
+                con = new ConnectionGene(b, a);
+            }
+
+            if (connections.Contains(con))
+            {
+                continue;
+            }
+            con = neat.GetConnection(con.From, con.To);
+
+            con.Weight = UnityEngine.Random.Range(-2f, 2f);
+            connections.Add(con);
+
+            return;
+        }
+    }
+
+    public void MutateNode()
+    {
+        ConnectionGene con = GetRandomConnect(connections);
+        if (con == null)
+        {
+            return;
+        }
+
+        NodeGene from = con.From;
+        NodeGene to = con.To;
+
+        NodeGene middle = neat.GetNode();
+        middle.X = (from.X + to.X) / 2;
+        middle.Y = (from.Y + to.Y) / 2;
+
+        ConnectionGene con1 = neat.GetConnection(from, middle);
+        ConnectionGene con2 = neat.GetConnection(middle, to);
+
+        con1.Weight = 1;
+        con2.Weight = con.Weight;
+        con2.Enabled = con.Enabled;
+
+        connections.Remove(con);
+        connections.Add(con1);
+        connections.Add(con2);
+
+        nodes.Add(middle);
+    }
+
+
+    public void MutateWeightShift()
+    {
+        ConnectionGene con = GetRandomConnect(connections);
+        if (con != null)
+        {
+            con.Weight += UnityEngine.Random.Range(-0.3f, 0.3f);
+        }
+    }
+
+    public void MutateWeightRandom()
+    {
+        ConnectionGene con = GetRandomConnect(connections);
+        if (con != null)
+        {
+            con.Weight = UnityEngine.Random.Range(-2f, 2f);
+        }
+    }
+
+    public void MutateLinkToggle()
+    {
+        ConnectionGene con = GetRandomConnect(connections);
+        if (con != null)
+        {
+            con.Enabled = !con.Enabled;
+        }
+    }
+
+    private ConnectionGene GetRandomConnect(List<ConnectionGene> genes)
+    {
+        int index = UnityEngine.Random.Range(0, genes.Count);
+        return genes[index];
+    }
+
+    private NodeGene GetRandomNode(List<NodeGene> genes)
+    {
+        int index = UnityEngine.Random.Range(0, genes.Count);
+        return genes[index];
     }
 }
